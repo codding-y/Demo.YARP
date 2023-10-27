00187d235e2380ca2044d954a26d471a6bdca9fc
阎鹏举 <@DESKTOP-28NV352>
2023-10-27 19:46:52 +0800

    init project

---
+++ MyProxy/Program.cs
@@ -0,0 +1,14 @@
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddReverseProxy()//添加ReverseProxy相关服务到DI
+                                  //从配置文件中加载ReverseProxy的设置
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+var app = builder.Build();
+
+app.MapGet("/hello", () => "Hello World!");
+
+app.MapReverseProxy();//使用ReverseProxy中间件
+
+app.Run();
